d726c39fd5fe9758569a937b2fc6da058c8e44c5
Aziz-K <73427214+Aziz-K@users.noreply.github.com>
2021-06-06 01:37:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #%%
 
-import preprocessing0
+
 import preprocessing
 
 path="german_credit_data.csv"
-df_credit = preprocessing0.preprocess_raw(path)
+df_credit = preprocessing.preprocess_raw(path)
 # %%
 print(df_credit)
 # %%
-X_train, X_test, y_train, y_test = preprocessing0.split_train_test(df_credit)
+X_train, X_test, y_train, y_test = preprocessing.split_train_test(df_credit)
 
 # %%
 preprocessing.pipeline(X_train, X_test, y_train, y_test)
